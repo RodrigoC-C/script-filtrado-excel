066c4e026da827a93a2dd4ab2ee3499cd4d68f51
RodrigoC-C <r.cerdacalapuja@uandresbello.edu>
2026-09-04 04:36:40 -0400

    Se realiza la incorporacion de nuevas variables de entornos, al igual que escribir las funciones escribir_en_hoja_historial para la gestion de historial de todas las herramientas en el excel maestro. al igual como crear la funcion guardar_y_cerrar con el motor openpyxl con load_workbook. se gestionan la data obtenida del excel analizar en un diccionario para luego ser agregado en la funcion escribir_en_hoja_historial

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # This is script for trasnlater and formting text of form to database of the tools on zone FME of Generation-V4
 import os
 import pandas as pd 
+from openpyxl import load_workbook
 from datetime import datetime as dt
 from dotenv import load_dotenv
 from extraccion import extraccion_herramientas
@@ -11,6 +12,10 @@ load_dotenv()
 # VARIABLE DE ENTORNOS
 archivo_guardado_id = os.getenv("ARCHIVO_GUARDADO_ID")
 excel_analizar = os.getenv("EXCEL_PATH")
+excel_maestro = os.getenv("EXCEL_MAESTRO_PATH")
+hoja_auditoria_maestro = os.getenv("HOJA_MAESTRO_AUDITORIA")
+
+#------------------- Lectura MD registro ---------------------------------------------------------------
 
 # Funcion de lectura del ultimo registro
 def lectura_ultimo_registro(nombre_archivo) -> int: 
@@ -35,8 +40,6 @@ def lectura_ultimo_registro(nombre_archivo) -> int:
 
     return(ultimo_id)
 
-
-
 # Funcion para anotar la ultima lectura del archivo id
 def escritura_ultimo_registro(nombre_archivo, id_recorrido, fecha_exacta):
 
@@ -47,12 +50,39 @@ def escritura_ultimo_registro(nombre_archivo, id_recorrido, fecha_exacta):
     
     return()
 
+# ------------------ Abrir y cerrar un excel maestro  -----------------------------------------
 
-def guardar_herramientas(herramienta, cantidad, hora):
+def escribir_en_hoja_historial(ruta_archivo, nombre_hoja, lista_diccionario):
+    # Abre excel en modo escritura
+    excel_archivo = load_workbook(filename=ruta_archivo)
+    # Escribe un valor en una celda y hoja específicas.
+    hoja = excel_archivo[nombre_hoja]
+    # Recorres tu lista de diccionarios
+    for movimiento in lista_diccionario:
+        # Extraes los valores en el orden EXACTO de tus columnas de Excel
+        fila_a_escribir = [
+            movimiento['Fecha Entrada/Salida'],
+            movimiento['Hora Entrada/Salida'],
+            movimiento['Nombre Herramienta'],
+            movimiento['Entrada/Salida'],
+            movimiento['Cantidad'],
+            movimiento['Empresa'],
+            movimiento['Persona (Opcional)']
+        ]
+        
+        # append() busca automáticamente la primera fila vacía al final y escribe los datos
+        hoja.append(fila_a_escribir)
 
-    return()
+    return excel_archivo
+
+def guardar_y_cerrar(excel_archivo, ruta_archivo):
+    # Guarda los cambios y cierra el archivo liberando memoria. 
+    excel_archivo.save(ruta_archivo)
+    excel_archivo.close()
+    print("Archivo guardado y cerrado exitosamente.")
 
 
+# ------------------------ Main -------------------------------------
 # Ultimo id 
 id_recorrido = lectura_ultimo_registro(archivo_guardado_id)
 # + 1 para el id siguiente
@@ -63,19 +93,20 @@ df = pd.read_excel(excel_analizar)
 # Busqueda de ultimo id 
 ultimo_id = df[df['ID'] == id_recorrido].index
 
-print(ultimo_id)
-
 if not ultimo_id.empty:
     indice = ultimo_id[0]
     # Cortamos la tabla usando iloc: desde la fila siguiente (+1) hasta el final
     df_nuevos = df.iloc[indice:]
-    print("test 1")
-    print(df_nuevos)
+
 else:
     # Si el ID no existe (ej. se borró o es tu primera vez corriendo el script), procesamos todo
     df_nuevos = df
 
-# 4. El Bucle: Recorremos solo las filas nuevas (reemplaza al While)
+
+# Donde guardaremos todos los diccionarios
+lista_diccionario = [] 
+
+# 4. El Bucle: Recorremos solo las filas nuevas
 # iterrows() avanza fila por fila manteniendo el "apuntador" automáticamente
 for index, fila in df_nuevos.iterrows():
     
@@ -91,20 +122,37 @@ for index, fila in df_nuevos.iterrows():
 Formato: [nombre herramienta, cantidad de herramienta, hora de registro.] 
 
 Ejemplo: cincel, 2, 07:30.                                         ...''']
-    
-    # Aquí es donde llamas a tu función personalizada
-    # resultado = mi_funcion_procesadora(id_actual, variable_a, variable_b)
-    
-    print(f"Procesando nuevo registro: {id_actual}, {entrada_salida}, {fecha_ingreso}, {empresa}, {persona}, {herramientas}")
 
-    extraccion_herramientas(herramientas)
+    # Extraemos las herramientas para iterarla de texto a listas
+    herramientas_lista = extraccion_herramientas(herramientas)
 
-# Capturar el momento exacto de la ejecución
-ahora = dt.now()
+    for item in herramientas_lista: 
+        diccionario_herramientas = {
+            "Fecha Entrada/Salida": fecha_ingreso.strftime("%d/%m/%Y") if fecha_ingreso else None,
+            "Hora Entrada/Salida":item[2] if item[2] else None,
+            "Nombre Herramienta": item[0],
+            "Entrada/Salida": entrada_salida,
+            "Cantidad": item[1],
+            "Empresa": empresa,
+            "Persona (Opcional)": persona if persona else None
+        }
+
+        # Por cada herramienta creamos un diccionario y lo mandamos agregamos a herramientas 
+        lista_diccionario.append(diccionario_herramientas)
+
+# Realizamos el guardado de cada diccionario en la hoja de auditoria 
+excel_modificado = escribir_en_hoja_historial(excel_maestro, hoja_auditoria_maestro, lista_diccionario)
+guardar_y_cerrar(excel_modificado, excel_maestro)
+        
+    
+    
+
+    
+
 # Darle un formato limpio (Día/Mes/Año Hora:Minuto:Segundo)
-fecha_exacta = ahora.strftime("%d/%m/%Y %H:%M")
+fecha_exacta = dt.now().strftime("%d/%m/%Y %H:%M")
 
 # Id recorrido sumandole 1 (aca se asignara el ultimo registro analizado
 id_recorrido = id_recorrido
 # Ultimo Id que se deja registro en el archivo para no caer en rebundancia
-#escritura_ultimo_registro(archivo_guardado_id, id_recorrido, fecha_exacta)
+escritura_ultimo_registro(archivo_guardado_id, id_recorrido, fecha_exacta)
