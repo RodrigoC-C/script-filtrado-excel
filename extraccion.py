9450b3a5cbddedb1881a1c0dfd65d47f52b6ebed
RodrigoC-C <r.cerdacalapuja@uandresbello.edu>
2026-09-04 03:36:29 -0400

    correccion de el formato que pasa la lista en la funcion extraccion_herramientas.

--- extraccion.py
+++ extraccion.py
@@ -4,9 +4,13 @@ def extraccion_herramientas(herramienta):
     texto_minuscula = herramienta.lower()
 
     pasadas = [p.strip() for p in texto_minuscula.split('.') if p.strip()] # Compresion de lista en python, es primera vez que lo veo
+    lista_anidada = [] # para guardar la lista y luego iterar sobre ella
 
     for i, pasada in enumerate(pasadas, start=1): # enumarate es para ahorrarse el 1 a fuera del bucle, ya que este se asigna en i
         elementos = [item.strip() for item in pasada.split(',')] # volvemos a ocupar compresion de lista pero para cortar los item y eliminar espacios
+        lista_anidada.append(elementos)
         print(f"Pasada {i}: {elementos}")
+
+    print(lista_anidada)
     
-    return()
+    return lista_anidada
